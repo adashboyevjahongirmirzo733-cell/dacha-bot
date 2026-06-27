4fc657dbef518a8e2b69286e2eee178dc563d991
adashboyevjahongirmirzo733-cell <adashboyevjahongirmirzo733@gmail.com>
2026-06-27 07:44:14 +0500

    Add card number to config.py
    Added a new configuration for card number.

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
-# ============================================================
-# SOZLAMALAR — Bu yerda o'zingizning ma'lumotlaringizni kiriting
-# ============================================================
-
-# Telegram bot token (@BotFather dan olgan)
 BOT_TOKEN = "8917124993:AAFa1x_OgQJIt5dOEHVyi2BACOa0yKGnHVw"
-
-# Sizning Telegram ID raqamingiz (@userinfobot dan olgan)
 ADMIN_ID = 8097790244
+CARD_NUMBER = "9860 0366 4392 0402"
 
-
-# To
 
